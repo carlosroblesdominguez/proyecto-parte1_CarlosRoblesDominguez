1f71d8f6e198804fcf6d34cd5328ee79e409e7c1
Carlos Robles <carlos.robles.dominguez.al@iespoligonosur.org>
2025-10-21 11:13:10 +0200

    voy a hacer las url desde 0

--- eventos_deportivos/urls.py
+++ eventos_deportivos/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('jugadores/', views.lista_jugadores, name='lista_jugadores'),
-    path('jugadores/<int:jugador_id>/', views.detalle_jugador, name='detalle_jugador'),
-    path('equipos/<int:equipo_id>/', views.detalle_equipo, name='detalle_equipo'),
-    path('partidos/', views.lista_partidos, name='lista_partidos')
+
 ]
