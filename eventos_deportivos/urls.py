1ecec673c0e395adf6b1f34f070a81f36dffbc27
carlosroblesdominguez <carlos.robles.dominguez.al@iespoligonosur.org>
2025-10-22 16:55:42 +0200

    Vista,URL y template creada, funcionando

--- eventos_deportivos/urls.py
+++ eventos_deportivos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    # URL 1: Listado de jugadores
+    path('jugadores/', views.ListaJugadores, name='lista_jugadores.html'),
 ]
--- eventos_deportivos/views.py
+++ eventos_deportivos/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import *
 
 # Create your views here.
 
 # Vista: Lista todos los jugadores con su equipo y estadísticas
+
+def ListaJugadores(request):
+    # QuerySet optimizado
+    jugadores = (
+        Jugador.objects
+        .select_related('estadisticas')  # OneToOne: EstadisticasJugador
+        .prefetch_related('equipo_set')  # ManyToMany vía EquipoJugador
+        .all()
+        .order_by('nombre')
+    )
+
+    # Equivalente SQL usando raw()
+    sql = """
+    SELECT j.id, j.nombre, j.apellido, j.fecha_nacimiento, j.posicion,
+            es.partidos_jugados, es.goles, es.asistencias, es.tarjetas,
+            e.id as equipo_id, e.nombre as equipo_nombre, ej.fecha_ingreso, ej.capitan
+    FROM eventos_deportivos_jugador j
+    INNER JOIN eventos_deportivos_estadisticasjugador es ON j.estadisticas_id = es.id
+    LEFT JOIN eventos_deportivos_equipojugador ej ON ej.jugador_id = j.id
+    LEFT JOIN eventos_deportivos_equipo e ON ej.equipo_id = e.id
+    ORDER BY j.nombre;
+    """
+    jugadores_sql = Jugador.objects.raw(sql)
+
+    contexto = {
+        "jugadores": jugadores, # Para usar QuerySet optimizado
+        "jugadores_sql": jugadores_sql  # Para mostrar que se puede usar raw()
+    }
+    return render(request, "eventos_deportivos/lista_jugadores.html", contexto)
