a9f792c34ba2e356692092239abc9c4c1c5e3945
carlosroblesdominguez <carlos.robles.dominguez.al@iespoligonosur.org>
2025-10-26 12:33:32 +0100

    .

--- eventos_deportivos/urls.py
+++ eventos_deportivos/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # URL1: Lista todos los jugadores
-    path('jugadores/', views.lista_jugadores, name='lista_jugadores'),
+    path('jugadores/', views.lista_jugadores, name='lista_jugadores'), # URL1: Lista todos los jugadores
     path('jugadores/<int:jugador_id>/', views.detalle_jugador, name='detalle_jugador'), #URL2 Detalles de un jugador
     path('equipos/<int:equipo_id>/', views.detalle_equipo, name='detalle_equipo'),  # URL3 detalles de un equipo
     path('partidos/', views.lista_partidos, name='lista_partidos'), # URL4 Lista de partidos
