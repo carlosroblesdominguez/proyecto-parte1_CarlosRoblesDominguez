150420cd4f2e35290ed4fe8be4c46bc52cf2795f
carlosroblesdominguez <carlos.robles.dominguez.al@iespoligonosur.org>
2025-11-15 11:53:28 +0100

    Static configurado, Plantilla html para los templates realizada

---
+++ eventos_deportivos/static/eventos_deportivos/js/app.js
@@ -0,0 +1,5 @@
+// scripts.js - comportamiento mínimo
+document.addEventListener('DOMContentLoaded', function(){
+  // ejemplo: imprimir en consola que el script está cargado
+  console.log('Scripts cargados - Eventos Deportivos');
+});
